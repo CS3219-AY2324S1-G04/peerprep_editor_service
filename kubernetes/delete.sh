bd5f326cc9422f5d2f11729d5d38da2c1febef15
Jedidiah Cheng <jedidiah940@gmail.com>
2023-11-13 18:07:31 +0800

    Update kubernetes config

--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-kubectl delete -f ./config_maps/redis_client_config.yaml
+kubectl delete -f ./config_maps/redis_client.yaml
 kubectl delete -f ./secrets/redis_client.yaml
+
+kubectl delete -f ./config_maps/api.yaml
 
 kubectl delete -f ./deployments/api.yaml
 kubectl delete -f ./services/api.yaml
--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-kubectl apply -f ./config_maps/redis_client_config.yaml
+kubectl apply -f ./config_maps/redis_client.yaml
 kubectl apply -f ./secrets/redis_client.yaml
+
+kubectl apply -f ./config_maps/api.yaml
 
 kubectl apply -f ./deployments/api.yaml
 kubectl apply -f ./services/api.yaml
